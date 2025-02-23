5eb7c06561938a52e2ff4bb80b85341dcd89e7b6
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-02-23 11:42:42 +0530

    Router Error Corrected

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,25 +1,14 @@
 
-import { useState, useEffect } from 'react';
-import Loading from '../../Pages/Loading/Loading';
-// import HeroSlider from '../../Pages/HeroSlider/HeroSlider';
-
-
 const Home = () => {
 
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => { 
-      setLoading(false);
-    }, 3000)
-  }, []);
-  if (loading) return <Loading />;
+
 
   return (
     <div className={"flex flex-col gap-0 mt-20"}>
-  
-       {/* <HeroSlider /> */}
-
+     
+     Hi
+       
+    
       
     </div>
   );
