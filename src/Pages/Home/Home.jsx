0f96e608e5d80f6bb6d423f9379006b4d0d9176b
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-02-23 11:10:15 +0530

    Routers Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,29 @@
+
+import { useState, useEffect } from 'react';
+import Loading from '../../Pages/Loading/Loading';
+// import HeroSlider from '../../Pages/HeroSlider/HeroSlider';
+
+
 const Home = () => {
-    
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => { 
+      setLoading(false);
+    }, 3000)
+  }, []);
+  if (loading) return <Loading />;
+
   return (
-    <div>Home</div>
-  )
+    <div className={"flex flex-col gap-0 mt-20"}>
+  
+       {/* <HeroSlider /> */}
+
+      
+    </div>
+  );
 }
 
 export default Home
+
